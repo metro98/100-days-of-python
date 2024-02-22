08c8db58f685b02773a9b62c5fc796ce7e728a26
Rahul <rahulshhahh@gmail.com>
2024-02-22 16:28:11 -0500

    finished oddeven.py program

--- Day3/find_ride.py
+++ Day3/find_ride.py
@@ -1,7 +1,7 @@
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
 
-if height > 120:
+if height >= 120:
     print("You can take a rollercoaster ride")
 else:
     print("Sorry you cannot take a rollercoster ride")
